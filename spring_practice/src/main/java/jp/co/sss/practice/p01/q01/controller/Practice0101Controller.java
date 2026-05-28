c979bc151e86f4968d465e6401517418d7c6f54d
yotakaha <yotakaha@C010423350.zipangu.cac.co.jp>
2026-05-28 17:29:01 +0900

    Practice1-01まで完了しました

--- spring_practice/src/main/java/jp/co/sss/practice/p01/q01/controller/Practice0101Controller.java
+++ spring_practice/src/main/java/jp/co/sss/practice/p01/q01/controller/Practice0101Controller.java
@@ -1,5 +1,13 @@
 package jp.co.sss.practice.p01.q01.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class Practice0101Controller {
 
+	@RequestMapping(path = "/opening")
+	public String ctrl() {
+		return "opening";
+	}
 }
